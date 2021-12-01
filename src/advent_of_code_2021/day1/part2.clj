7f862c1b4f0fe26ffc3452f60a0beefebe1112a6
sukhiboi <41sukhdevsingh@gmail.com>
2021-12-01 18:57:38 +0530

    Completed day1 part2

--- src/advent_of_code_2021/day1/part2.clj
+++ src/advent_of_code_2021/day1/part2.clj
@@ -1,3 +1,9 @@
 (in-ns 'advent-of-code-2021.day1.core)
 
-(defn part2 [] (str "Day1 Part2"))
+(defn part2 []
+  (->> (parse-input)
+       (partition 3 1)
+       (map #(reduce + %1))
+       (partition 2 1)
+       (reduce #(if (apply < %2) (inc %1) %1) 0)
+       println))
